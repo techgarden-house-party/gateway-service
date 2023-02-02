07d9c8d822e34db645926483a3b5002929e650b1
TechGardenCode <techgardencode@gmail.com>
2023-02-02 13:04:05 -0500

    feat: initial commit

---
+++ src/main/java/gg/techgarden/eventapp/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package gg.techgarden.eventapp.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf().disable()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+}
---
+++ src/main/java/gg/techgarden/eventapp/gateway/controller/GatewayController.java
@@ -0,0 +1,56 @@
+package gg.techgarden.eventapp.gateway.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin
+public class GatewayController {
+    @RequestMapping("/{serviceName}/**")
+    public ResponseEntity mirrorRest(@RequestBody(required = false) String body, @PathVariable String serviceName,
+                                     HttpMethod method, HttpServletRequest request, HttpServletResponse response)
+            throws URISyntaxException {
+        String requestUrl = request.getRequestURI();
+
+        log.info("Request Url: {}", requestUrl);
+
+        URI uri = new URI("http", null, serviceName + "-service", 8080, null, null, null);
+        uri = UriComponentsBuilder.fromUri(uri)
+                .path(requestUrl)
+                .query(request.getQueryString())
+                .build(true).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.set(headerName, request.getHeader(headerName));
+        }
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            return restTemplate.exchange(uri, method, httpEntity, String.class);
+        } catch(HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
+    }
+}
